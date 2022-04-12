73c8c2bb8215f8629a8df14b0a3295e27aeea935
hayeong <hayeong0624@gmail.com>
2022-04-12 19:21:38 +0900

    Level1_K번째 수

--- src/sort/sort.java
+++ src/sort/sort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import java.util.*;
+
 /*
 	배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
 	예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
@@ -14,16 +16,36 @@ package sort;
 */
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-    	
-    	for(int i = 0; i < commands.length; i++) {
-    		
-    	}
-        int[] answer = {};
+	public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0; i < commands.length; i++) {
+            List<Integer> list = new ArrayList<Integer>();
+        
+            for(int j = commands[i][0]-1; j < commands[i][1]; j++) {
+                list.add(array[j]);
+            }
+            
+            Collections.sort(list);
+            
+            answer[i] = list.get(commands[i][2]-1);
+        }
+        
         return answer;
     }
 }
 
 /*
+	정확성  테스트
+	테스트 1 〉	통과 (0.19ms, 73.3MB)
+	테스트 2 〉	통과 (0.20ms, 77.1MB)
+	테스트 3 〉	통과 (0.22ms, 73.1MB)
+	테스트 4 〉	통과 (0.19ms, 73.3MB)
+	테스트 5 〉	통과 (0.20ms, 75.4MB)
+	테스트 6 〉	통과 (0.25ms, 73.3MB)
+	테스트 7 〉	통과 (0.22ms, 74.7MB)
 	
+	채점 결과
+	정확성: 100.0
+	합계: 100.0 / 100.0
 */
